18c59cd9f721e83bc5fd16b257d82f864365ac80
kanakeswar-2000 <pkanakeswarareddy@gmail.com>
2026-05-20 13:00:06 +0530

    button styling and action disappear

--- js-assignments/assignment-12/index.js
+++ js-assignments/assignment-12/index.js
@@ -8,24 +8,46 @@ let historyBtnEle=document.getElementById("history-btn")
 let historyConEle=document.getElementById("history-con")
 
 depositBtnEle.addEventListener("click",()=>{
+    // button styling
+    depositBtnEle.classList.add("active-button")
+    withdrawBtnEle.classList.remove("active-button")
+    balanceBtnEle.classList.remove("active-button")
+    historyBtnEle.classList.remove("active-button")
+
+    // actions display and removal
     depositConEle.classList.remove("deposit-con")
-    withdrawConEle.classList.add("withdraw-btn")
+    withdrawConEle.classList.add("withdraw-con")
     balanceConEle.classList.add("balance-con")
     historyConEle.classList.add("history-con")
 })
 withdrawBtnEle.addEventListener("click",()=>{
+    withdrawBtnEle.classList.add("active-button")
+    depositBtnEle.classList.remove("active-button")
+    balanceBtnEle.classList.remove("active-button")
+    historyBtnEle.classList.remove("active-button")
+
     depositConEle.classList.add("deposit-con")
     withdrawConEle.classList.remove("withdraw-con")
     balanceConEle.classList.add("balance-con")
     historyConEle.classList.add("history-con")
 })
 balanceBtnEle.addEventListener("click",()=>{
+    balanceBtnEle.classList.add("active-button")
+    depositBtnEle.classList.remove("active-button")
+    withdrawBtnEle.classList.remove("active-button")
+    historyBtnEle.classList.remove("active-button")
+
     depositConEle.classList.add("deposit-con")
     withdrawConEle.classList.add("withdraw-con")
     balanceConEle.classList.remove("balance-con")
     historyConEle.classList.add("history-con")
 })
 historyBtnEle.addEventListener("click",()=>{
+    historyBtnEle.classList.add("active-button")
+    depositBtnEle.classList.remove("active-button")
+    withdrawBtnEle.classList.remove("active-button")
+    balanceBtnEle.classList.remove("active-button")
+
     depositConEle.classList.add("deposit-con")
     withdrawConEle.classList.add("withdraw-con")
     balanceConEle.classList.add("balance-con")
@@ -61,9 +83,11 @@ class Atm{
 const atm= new Atm("",0,[])
 
 function handleDeposit(){
+    depositConEle.classList.add("deposit-con")
     atm.makeDeposit()
 }
 function handleWithdrawal(){
+    withdrawConEle.classList.add("withdraw-con")
     atm.makeWithDrawal()
 }
 
